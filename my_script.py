db18454b6ee9a448320e022d5fb295a86d081e30
Kamden Watterberg <kwatterb@asu.edu>
2024-11-03 21:54:42 -0700

    First Commit

---
+++ my_script.py
@@ -0,0 +1,10 @@
+#Kamden Watterberg Square of Number Script
+
+def calculate_square(number):
+    """Returns the square of a number."""
+    return number * number
+
+# Example usage
+if __name__ == "__main__":
+    num = 5
+    print(f"The square of {num} is {calculate_square(num)}")
